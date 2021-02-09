29d064d4c42c027cf416f729b5009a2d6785c7fe
22sdhoat <22sdhoat@athenian.org>
2021-02-09 05:41:21 +0000

    completed methods

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,19 +1,44 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MyMain {
 
     // Sorts the ArrayList using insertion sort
     public static ArrayList<Integer> insertionSort(ArrayList<Integer> list) {
-        // YOUR CODE HERE
-        return null;
+        for (int i = 1; i < list.size(); i++) {
+            int next = list.get(i);
+            int j = i - 1;
+            while ((j > -1) && (list.get(j).compareTo(next) == 1)) {
+                list.set(j + 1, list.get(j));
+                j--;
+            }
+            list.set(j + 1, next);
+        }
+        return list;
     }
 
      // Merges two sorted arrays into one large combined
      // sorted array
      // You may assume arr1 and arr2 are the same length
     public static int[] merge(int[] arr1, int[] arr2) { 
-        // YOUR CODE HERE
-        return null;
+        int[] merge = new int[arr1.length + arr2.length];
+        int i = 0;
+        int j = 0;
+        int k = 0; 
+                 
+        while (i < arr1.length) {
+            merge[k] = arr1[i];
+            i++;
+            k++;
+        } 
+                 
+        while (j < arr2.length) {
+            merge[k] = arr2[j];
+            j++;
+            k++;
+        } 
+        Arrays.sort(merge);
+        return merge;
     }
 
     public static void main(String[] args) {
